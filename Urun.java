e24f712ea72e64fa656a0ac27bbb56989877d407
ASLI258 <asli.celikk2024@gmail.com>
2026-05-12 22:47:57 +0300

    Update Urun.java

--- Urun.java
+++ Urun.java
@@ -1,9 +1,8 @@
-public class Urun {
-
+public class Urun{
     String urunAdi;
     double fiyat;
 
-    public Urun(String urunAdi, double fiyat) {
+    public Urun(String urunAdi, double fiyat){
         this.urunAdi = urunAdi;
         this.fiyat = fiyat;
     }
