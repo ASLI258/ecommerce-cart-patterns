1904d9d204305530a9182bf7ddcb212ee27a9030
ASLI258 <asli.celikk2024@gmail.com>
2026-05-12 22:28:26 +0300

    Create Sepet.java

---
+++ Sepet.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Sepet {
+
+    ArrayList<Urun> urunler = new ArrayList<>();
+
+    public void urunEkle(Urun urun) {
+        urunler.add(urun);
+    }
+
+    public double odemeYap(String indirimTuru, String odemeTuru) {
+
+        double toplam = 0;
+
+        for (Urun urun : urunler) {
+            toplam += urun.fiyat;
+        }
+
+        // Kötü tasarım: if-else zinciri
+        if (indirimTuru.equals("OGRENCI")) {
+            toplam = toplam * 0.90;
+        }
+        else if (indirimTuru.equals("VIP")) {
+            toplam = toplam * 0.80;
+        }
+        else if (indirimTuru.equals("YILBASI")) {
+            toplam = toplam * 0.85;
+        }
+
+        // Kötü tasarım: ödeme tipi kontrolü
+        if (odemeTuru.equals("KART")) {
+            System.out.println("Kart ile ödeme yapıldı.");
+        }
+        else if (odemeTuru.equals("NAKIT")) {
+            System.out.println("Nakit ödeme yapıldı.");
+        }
+
+        return toplam;
+    }
+}
