5e55e2211be24ea0dadb9a0d52cd8673649c4ec4
ASLI258 <asli.celikk2024@gmail.com>
2026-05-12 22:24:41 +0300

    Create Urun.java

---
+++ Urun.java
@@ -0,0 +1,10 @@
+public class Urun {
+
+    String urunAdi;
+    double fiyat;
+
+    public Urun(String urunAdi, double fiyat) {
+        this.urunAdi = urunAdi;
+        this.fiyat = fiyat;
+    }
+}
