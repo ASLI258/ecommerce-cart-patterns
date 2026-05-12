0337a7b2ea429c5612fa3d510e8cc30341361c80
ASLI258 <asli.celikk2024@gmail.com>
2026-05-12 22:47:35 +0300

    Update Sepet.java

--- Sepet.java
+++ Sepet.java
@@ -1,40 +1,33 @@
 import java.util.ArrayList;
-
-public class Sepet {
-
+public class Sepet{
     ArrayList<Urun> urunler = new ArrayList<>();
 
-    public void urunEkle(Urun urun) {
+    public void urunEkle(Urun urun){
         urunler.add(urun);
     }
-
-    public double odemeYap(String indirimTuru, String odemeTuru) {
-
+    public double odemeYap(String indirimTuru, String odemeTuru){
         double toplam = 0;
 
-        for (Urun urun : urunler) {
+        for(Urun urun : urunler){
             toplam += urun.fiyat;
         }
-
-        // Kötü tasarım: if-else zinciri
-        if (indirimTuru.equals("OGRENCI")) {
+        
+        if(indirimTuru.equals("OGRENCI")){
             toplam = toplam * 0.90;
         }
-        else if (indirimTuru.equals("VIP")) {
+        else if(indirimTuru.equals("VIP")){
             toplam = toplam * 0.80;
         }
-        else if (indirimTuru.equals("YILBASI")) {
+        else if(indirimTuru.equals("YILBASI")){
             toplam = toplam * 0.85;
         }
 
-        // Kötü tasarım: ödeme tipi kontrolü
-        if (odemeTuru.equals("KART")) {
+        if (odemeTuru.equals("KART")){
             System.out.println("Kart ile ödeme yapıldı.");
         }
-        else if (odemeTuru.equals("NAKIT")) {
+        else if(odemeTuru.equals("NAKIT")){
             System.out.println("Nakit ödeme yapıldı.");
         }
-
         return toplam;
     }
 }
