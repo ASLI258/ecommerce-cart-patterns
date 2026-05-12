02643217904d7bd4371a854f6da455be18fa581b
ASLI258 <asli.celikk2024@gmail.com>
2026-05-12 22:43:57 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,13 @@
-public class Main {
-
-    public static void main(String[] args) {
-
+public class Main{
+    public static void main(String[] args){
+        
         Sepet sepet = new Sepet();
-
+        
         sepet.urunEkle(new Urun("Laptop", 30000));
         sepet.urunEkle(new Urun("Mouse", 500));
 
         double toplam = sepet.odemeYap("VIP", "KART");
-
+        
         System.out.println("Toplam Tutar: " + toplam);
     }
 }
